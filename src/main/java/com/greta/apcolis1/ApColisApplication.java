7581e28f8dcf3345bb23657838622a4dd309894f
Miloww <theo.mourocq@gretacotesarmor.fr>
2023-10-30 14:58:02 +0100

    first commit

---
+++ src/main/java/com/greta/apcolis1/ApColisApplication.java
@@ -0,0 +1,13 @@
+package com.greta.apcolis1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApColisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApColisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greta/apcolis/ApColisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greta.apcolis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApColisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
